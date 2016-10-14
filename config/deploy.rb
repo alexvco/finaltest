f5cd5dc684ad6c74c2ddcf45817e5ad732dbe5ca
alexv7 <alexvarjabedian7@gmail.com>
2016-10-13 20:53:19 -0400

    corrected deploy_to

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.1'
 set :application, 'finaltest'
 set :repo_url, 'git@github.com:alexvco/finaltest.git'
 
-set :deploy_to, '/home/deploy/do2'
+set :deploy_to, '/home/deploy/finaltest'
 set :rbenv_ruby, '2.3.1'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
